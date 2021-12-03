9105e62aebace892f8a4cac75d4d1b9d899ae2db
ramiro-luengo <ramirobmds@gmail.com>
2021-12-03 12:32:44 -0300

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Header from '../components/header'
+import Main from '../components/main'
+import Nav from '../components/nav'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <Header />
+
+      {/**** MAIN ****/}
+
+      <Main />
+
+      {/**** FOOTER ****/}
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
+    </div>
+  )
+}
